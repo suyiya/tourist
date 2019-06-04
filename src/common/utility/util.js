0ec9a0a65820c464af536e6a540b70d97a8c390c
suyiya <542258512@qq.com>
2019-06-04 23:24:28 +0800

    update

--- src/common/utility/util.js
+++ src/common/utility/util.js
@@ -4,7 +4,7 @@ window.util = {
      */
     getUrlParam: (name) => {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+        var r = window.location.hash.substr(1).match(reg);  //匹配目标参数
         if (r != null) return decodeURI(r[2]); return null; //返回参数值
     },
     /**
